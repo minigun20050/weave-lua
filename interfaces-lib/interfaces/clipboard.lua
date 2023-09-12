9d76c5d1f847e004c0dbc3b8cd286e151b296642
LiterallyNN <LiterallyLooser@gmail.com>
2023-09-13 01:34:51 +0300

    yes.

--- interfaces-lib/interfaces/clipboard.lua
+++ interfaces-lib/interfaces/clipboard.lua
@@ -1,3 +1,10 @@
+--[[
+    Part of interfaces library for weave.
+
+    Source:
+      Random leaked neverlose lua code ;D
+--]]
+
 local ffi = require("ffi")
 local base = require("interfaces/__base")
 
